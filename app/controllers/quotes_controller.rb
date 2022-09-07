1b5b3cb49b184a593d6ac9f16801b025330a6cd8
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-09-06 20:38:51 -0400

    Error messages display and flash

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,11 +9,16 @@ class QuotesController < ApplicationController
   end
 
   def new
+    @quote = Quote.new
   end
 
   def create
     @quote = Quote.new(params.require(:quote).permit(:content))
-    @quote.save
-    redirect_to @quote
+    if @quote.save
+      flash[:notice] = "Quote was created successfully."
+      redirect_to @quote
+    else
+      render 'new'
+    end
   end
 end

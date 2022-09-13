8f633861b49cc21757cd13dd948b816e2e597676
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-09-12 21:47:33 -0400

    Adding gravatar icons to the Authors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def gravatar_for(author, options = { size: 200 })
+    email_address = author.first_name.downcase + "@gmail.com"
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    style = options[:style]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=robohash&r=x"
+    image_tag(gravatar_url, alt: author.full_name, class: "rounded shadow mx-auto d-block")
+  end
 end

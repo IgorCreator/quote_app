cec4e2f9bb2710fb8192a71d3f4c7660c2e2a4ac
Igor_Creator <asigoria@gmail.com>
2023-03-30 21:41:41 -0400

    Adding additional logic for Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,17 +1,34 @@
 class Author < ApplicationRecord
   before_save do
-    self.first_name = first_name.capitalize
-    self.last_name = last_name.capitalize if self.last_name
+    normalized_name =  Nameable.parse(
+      [prefix, first_name, middle_name, last_name].join(' ') + (suffix ? ", #{suffix}" : '')
+    )
+    self.first_name = normalized_name.to_firstname
+    self.middle_name = normalized_name.to_middlename if normalized_name.middle != nil
+    self.last_name = normalized_name.to_lastname if normalized_name.last != nil
+    self.prefix = normalized_name.to_prefix if normalized_name.prefix != nil
+    self.suffix = normalized_name.to_suffix if normalized_name.suffix != nil
   end
 
   has_many :quotes, dependent: :destroy
-  validates :first_name, presence: true
 
+  validates :first_name, presence: true
   validates_uniqueness_of :first_name,
                           :scope => :last_name,
                           :message => "and Last name already exist"
 
   def full_name
-    [first_name, middle_name, last_name].join(' ')
+    [prefix, first_name, middle_name, last_name].join(' ') + (suffix ? ", #{suffix}" : '')
+  end
+
+  def self.create_or_find_by_full_name(full_name)
+    parsed_name = Nameable.parse(full_name)
+    self.create_or_find_by(
+      prefix: parsed_name.prefix,
+      first_name: parsed_name.first,
+      middle_name: parsed_name.middle,
+      last_name: parsed_name.last,
+      suffix: parsed_name.suffix
+    )
   end
 end
